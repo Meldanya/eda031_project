7c121653bb1dbe3da0cb02af4a999c6724da6f77
Martin Trasteby <trasteby@outlook.com>
2013-03-26 15:07:30 +0100

    slight tweak of test

--- src/main.cc
+++ src/main.cc
@@ -3,11 +3,19 @@
 #include <iterator>
 #include <algorithm>
 
+using namespace std;
+
 int main() {
 	mem_database db = mem_database();
 	db.create_ng("ng1");
 	db.create_ng("ng2");
 	
-	std::vector<ng> ngs = db.list_ng();
-	std::copy(ngs.begin(), ngs.end(), std::ostream_iterator<ng>(std::cout));
+	vector<ng> ngs = db.list_ng();
+	copy(ngs.begin(), ngs.end(), ostream_iterator<ng>(cout));
+	
+	db.create_art(ngs[0].id, "T-Bone", "Snel hest", "Jak er snel hest\nDERP");
+	vector<art> arts = db.list_art(ngs[0].id);
+	copy(arts.begin(), arts.end(), ostream_iterator<art>(cout));
+	
+	art a = db.get_art(ngs[0].id, 0);
 }
