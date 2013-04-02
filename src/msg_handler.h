6216bf9612a28d520a37734ca1787f1048ae9b7d
Tommy Olofsson <tommy.olofsson.90@gmail.com>
2013-04-02 13:21:47 +0200

    Doc comments are apparently required for this.

--- src/msg_handler.h
+++ src/msg_handler.h
@@ -12,21 +12,62 @@ class msg_handler
 	public:
 		typedef struct malformed_req_exception {} malformed_req_exception;
 
+		/**
+		 * Creates an instance for handling messages on a specified connection.
+		 *
+		 * @param conn The connection th handle messages on.
+		 */
 		msg_handler(client_server::Connection *conn) : conn(conn) {}
 
 	protected:
 		client_server::Connection *conn;
 
+		/**
+		 * Reads the end byte of the protocol from the channel.
+		 *
+		 * TODO: Move to server_msg_handler.
+		 */
 		void read_end(); //throw(client_server::ConnectionClosedException);
-		int read_int(); //throw(client_server::ConnectionClosedException);
+
+		/**
+		 * Read a four byte int from the connection.
+		 *
+		 * @return The read integer.
+		 */
+		int read_int();
+
+		/**
+		 * Reads a string in protocol format from the connection.
+		 */
 		std::string read_parameter_string();
-			//throw(client_server::ConnectionClosedException, malformed_req_exception);
-		int read_parameter_int(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void write_int(int value); //throw(client_server::ConnectionClosedException);
-		void write_string(const std::string &s); //throw(client_server::ConnectionClosedException,
-					//malformed_req_exception);
-		void write_parameter_int(int value); //throw(client_server::ConnectionClosedException);
+
+		/**
+		 * Reads an integer in protocol format from the connection.
+		 *
+		 * @return The read integer.
+		 */
+		int read_parameter_int();
+
+		/**
+		 * Writes a four byte integer to the connection.
+		 * 
+		 * @param value The integer to write.
+		 */
+		void write_int(int value);
+
+		/**
+		 * Writes a string in protocol format to the connection.
+		 *
+		 * @param s The string to write.
+		 */
+		void write_string(const std::string &s);
+
+		/**
+		 * Writes an integer in protocol format to the connection.
+		 *
+		 * @param value The integer to write.
+		 */
+		void write_parameter_int(int value);
 };
 
 #endif
--- src/server_msg_handler.h
+++ src/server_msg_handler.h
@@ -7,28 +7,58 @@
 class server_msg_handler : public msg_handler
 {
 	public:
-		void handle(); //throw(client_server::ConnectionClosedException);
+	/**
+	 * Handle a request arriving on connection.
+	 */
+		void handle();
 
+		/**
+		 * Create an instance capable of handlinge requests on conn using
+		 * database db.
+		 *
+		 * @param conn The connection to handle requests on.
+		 * @param db   The database used to create responses.
+		 */
 		server_msg_handler(client_server::Connection *conn, database &db)
 			: msg_handler(conn), db(db) {}
 
 	private:
 		database &db;
 
-		void handle_list_ng(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_create_ng(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_delete_ng(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_list_art(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_create_art(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_delete_art(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
-		void handle_get_article(); //throw(client_server::ConnectionClosedException,
-				//malformed_req_exception);
+		/**
+		 * Handles a request for news group listing.
+		 */
+		void handle_list_ng();
+
+		/**
+		 * Handles a request to create a newsgroup.
+		 */
+		void handle_create_ng();
+
+		/**
+		 * Handles a request to delete a newsgroup.
+		 */
+		void handle_delete_ng();
+
+		/**
+		 * Handles a request for a listing of the articles in a newsgroup.
+		 */
+		void handle_list_art();
+
+		/**
+		 * Handles a request to create an article.
+		 */
+		void handle_create_art();
+
+		/**
+		 * Handles a request to delete an article.
+		 */
+		void handle_delete_art();
+
+		/**
+		 * Handles a request to get an article.
+		 */
+		void handle_get_article();
 };
 
 #endif
